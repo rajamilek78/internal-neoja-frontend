ac7dd3199fd313eb8505d90a81baabfea307f60b
Shraddha Bhad <bhadshraddha16@gmail.com>
2024-02-05 14:20:30 +0530

    table-format-one updated

--- src/app/private-modules/modules/league-module/components/league-container/league-container.component.ts
+++ src/app/private-modules/modules/league-module/components/league-container/league-container.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-league-container',
   templateUrl: './league-container.component.html',
-  styleUrl: './league-container.component.scss'
+  styleUrl: './league-container.component.scss',
 })
-export class LeagueContainerComponent {
-
-}
+export class LeagueContainerComponent {}
