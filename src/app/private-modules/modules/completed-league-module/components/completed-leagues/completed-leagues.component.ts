d27da2056df700d7ada8a5273ca0e64c0a1f009b
ravirajsinh07 <ravirajsinhrathod50@gmail.com>
2024-02-17 16:32:00 +0530

    --amend

--- src/app/private-modules/modules/completed-league-module/components/completed-leagues/completed-leagues.component.ts
+++ src/app/private-modules/modules/completed-league-module/components/completed-leagues/completed-leagues.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonService } from '../../../../../core';
+import { CommonService, SharedService } from '../../../../../core';
 import { CompanyModel } from '@app/helpers/models/company.model';
 import { MatDialog } from '@angular/material/dialog';
 import { LockDataDialogueComponent } from '../../../league-module/components/lock-data-dialogue/lock-data-dialogue.component';
 import { Router } from '@angular/router';
 import { RouteConstant } from '@app/helpers/constants';
+import { Subscription } from 'rxjs';
+import { UserModel } from '@app/helpers/models';
 @Component({
   selector: 'app-completed-leagues',
   templateUrl: './completed-leagues.component.html',
   styleUrl: './completed-leagues.component.scss',
 })
 export class CompletedLeaguesComponent implements OnInit {
+  userDetailSub$!: Subscription;
+  userDetail!: UserModel | null;
+  leagues : any[] = [];
+  selectedLeague! : string;
   companies: { [key: string]: { name: string; description: string } } = {};
   newCompanies!: CompanyModel[];
   clubs: { [key: string]: { name: string; phone: string; address: any } } = {};
@@ -18,7 +24,8 @@ export class CompletedLeaguesComponent implements OnInit {
   constructor(
     private commonService: CommonService,
     private dialog: MatDialog,
-    private router : Router
+    private router : Router,
+    private sharedUserService : SharedService
   ) {}
 
   openDialogue(): void {
@@ -31,7 +38,9 @@ export class CompletedLeaguesComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getAllCompanies();
+    // this.getAllCompanies();
+    this.userSubscriber();
+    this.getAllLeagues();
   }
 
   viewScore(){
@@ -42,6 +51,30 @@ export class CompletedLeaguesComponent implements OnInit {
     // this.router.navigate([RouteConstant.LEAGUE_CONTAINER]);
     this.router.navigate(['players-league' ,{ edite: true }]);
   }
+
+  userSubscriber = () => {
+    this.userDetailSub$ = this.sharedUserService.getUserDetailCall()
+      .subscribe(() => {
+        this.userDetail = this.sharedUserService.getUser();
+        console.log(this.userDetail);
+      });
+  };
+  getAllLeagues() {
+    const companyID = this.userDetail?.owned_companies;
+    const clubID = this.userDetail?.owned_clubs;
+    const companyIDclubIDStr = `${companyID}/${clubID}/all`
+    this.commonService.getAllLeagues(`${companyIDclubIDStr}`).subscribe({
+      next: (res: any) => {
+        this.leagues = Object.keys(res).map(key => ({id: key, name: res[key].name}));
+        this.selectedLeague = this.leagues[0].id;
+        console.log(this.leagues);
+        },
+      error: (err: any) => {
+        console.log(err);
+      }
+    });
+  }
+
 
   getAllCompanies() {
     this.commonService.getAllCompanies().subscribe({
