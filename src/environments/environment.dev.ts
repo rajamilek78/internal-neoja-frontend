d741fb5041273e91bd80363ad527f572f3603829
ravirajsinh07 <ravirajsinhrathod50@gmail.com>
2024-02-19 10:57:26 +0530

    enviroenment file updated to change baseurl

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,5 +3,5 @@ export const environment = {
     demo: false,
     dev: true,
     apiUrl: 'https://build-round-hy3odlnrxq-ue.a.run.app',
-    companyUrl: 'https://elo-db-operations-hy3odlnrxq-ue.a.run.app',
+    companyUrl: 'https://elo-db-operations-scx4xn2oqq-ue.a.run.app',
   };
