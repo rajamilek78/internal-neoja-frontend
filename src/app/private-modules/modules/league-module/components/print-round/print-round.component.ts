0ca6596cb7fd0a6d2734e4edbbaf137642a9c37e
ravirajsinh07 <ravirajsinhrathod50@gmail.com>
2024-02-24 13:39:52 +0530

    remove disable button for view score button

---
+++ src/app/private-modules/modules/league-module/components/print-round/print-round.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-print-round',
+  templateUrl: './print-round.component.html',
+  styleUrl: './print-round.component.scss'
+})
+export class PrintRoundComponent {
+
+}
