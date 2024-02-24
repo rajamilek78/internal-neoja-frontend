30cf1073407697e60a72b4b4886e9e46361398cc
ravirajsinh07 <ravirajsinhrathod50@gmail.com>
2024-02-24 13:41:33 +0530

    disable button removed from completed for view score

--- src/app/private-modules/modules/league-module/league-routing.module.ts
+++ src/app/private-modules/modules/league-module/league-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LeagueContainerComponent } from './components';
+import { LeagueContainerComponent, PrintRoundComponent } from './components';
 
 const routes: Routes = [
   {
     path: '',
     component: LeagueContainerComponent,
   },
+  { path : 'print', component : PrintRoundComponent}
 ];
 
 @NgModule({
