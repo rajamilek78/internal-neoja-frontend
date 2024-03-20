c06632e7d73d9c09d963ae3e98318230cdbe5704
ravirajsinh07 <ravirajsinhrathod50@gmail.com>
2024-03-20 15:22:53 +0530

    environment .ts chagned

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,10 +2,8 @@ export const environment = {
   production: false,
   demo: false,
   dev: true,
-  // eloBuildRoundURL: 'https://elo-build-round-scx4xn2oqq-ue.a.run.app',
-  // eloDbOperationURL: 'https://elo-db-operations-scx4xn2oqq-ue.a.run.app',
-  eloBuildRoundURL: "https://elo-build-round-2dzegz3xeq-ue.a.run.app",
-    eloDbOperationURL: "https://elo-db-operations-2dzegz3xeq-ue.a.run.app",
+  eloBuildRoundURL: 'https://elo-build-round-scx4xn2oqq-ue.a.run.app',
+  eloDbOperationURL: 'https://elo-db-operations-scx4xn2oqq-ue.a.run.app',
   // eloBuildRoundURL: 'http://localhost:8080',
   // eloDbOperationURL: 'http://localhost:8081',
 };
